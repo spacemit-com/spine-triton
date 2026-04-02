a25d1fbebf7889d228f87878d0e8a17c74102408
zuoweixia <weixia.zuo@spacemit.com>
2026-04-02 17:56:09 +0800

    fix(build): set LLVM_SYSPATH for offline wheel build

--- scripts/build_whl.sh
+++ scripts/build_whl.sh
@@ -1,10 +1,12 @@
-# !/bin/bash
+#!/bin/bash
+set -euo pipefail
 # bash build_whl.sh ${LLVM_INSTALL_DIR} {arch/x86_64/riscv64} {spine-mlir-install-dir}
 
 # Resolve all input paths to absolute paths to avoid breakage after pushd
 LLVM_INSTALL_DIR=$(cd "${1}" && pwd)
 BUILD_DIR=build-wheel-${2}
 SPINE_MLIR_INSTALL_DIR=$(cd "${3}" && pwd)
+chmod a+x "${SPINE_MLIR_INSTALL_DIR}"/bin/*
 
 CUR_DIR=${PWD}
 VERSION_NUMBER=$(cat VERSION_NUMBER)
@@ -32,7 +34,7 @@ export CC=${RISCV_ROOT_PATH}/bin/riscv64-unknown-linux-gnu-gcc
 export CXX=${RISCV_ROOT_PATH}/bin/riscv64-unknown-linux-gnu-g++
 
 TRITON_BUILD_PROTON=false TRITON_BUILD_WITH_CLANG_LLD=false TRITON_BUILD_UT=false TRITON_OFFLINE_BUILD=true \
-TRITON_BUILD_WITH_CCACHE=false LLVM_ROOT_DIR=${LLVM_INSTALL_DIR} MAX_JOBS=${MAX_JOBS} \
+TRITON_BUILD_WITH_CCACHE=false LLVM_ROOT_DIR=${LLVM_INSTALL_DIR} LLVM_SYSPATH=${LLVM_INSTALL_DIR} MAX_JOBS=${MAX_JOBS} \
 python3 setup.py bdist_wheel --plat=linux-${2}
 
 cp dist/*.whl ${CUR_DIR}/${BUILD_DIR}/
